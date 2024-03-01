f44b438a7a79ad3ea0570051812ce299e09bae35
mccartheney <mccartheney@hotmail.com>
2024-03-01 19:02:14 +0000

    Header finalized

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./sass/style.css"
 
 const root = ReactDOM.createRoot (document.getElementById("root"));
-root.render (
-  <>
-    <h1>
-      Hello
-    </h1>
-  </>
-);
+root.render (<App/>);
